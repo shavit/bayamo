f51d05bda1afc900b86d00ccb3466c39cfaa88ae
shavit <shavit@users.noreply.github.com>
2017-12-27 01:40:00 -0500

    🙊

--- downloader.go
+++ downloader.go
@@ -1,9 +1,27 @@
 package bayamo
 
-type Downloader interface {}
+import (
+  "os"
+)
 
+type Downloader interface {
+  // Download data from url into a file
+  Get(url string) (f *os.File, err error)
+}
+
+//
+//  fileDownloader
+//
 type fileDownloader struct {}
 
+// Download data from url into a file
+func (dwn *fileDownloader) Get(url string) (f *os.File, err error){
+  return f, err
+}
+
+//
+//  Downloader
+//
 func NewDownloader() Downloader {
   return new(fileDownloader)
 }
--- downloader_test.go
+++ downloader_test.go
@@ -11,3 +11,13 @@ func TestCreateDownloader(t *testing.T){
     t.Error("Found nil while creating a downloader")
   }
 }
+
+func TestFileDownloaderGet(t *testing.T){
+  var dwn Downloader = NewDownloader()
+  var err error
+
+  _, err = dwn.Get("")
+  if err != nil {
+    t.Error("Error downloading a file: " + err.Error())
+  }
+}
